f554ca5a0494fc633a2be0dd6a771029530f25f5
Samuel Paulíny <pauliny@cngroup.dk>
2022-03-10 20:01:19 +0100

    add authentication add database chat history

---
+++ ChatAppBackend/ChatAppBackend/Controllers/UserController.cs
@@ -0,0 +1,37 @@
+﻿using ChatAppBackend.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ChatAppBackend.Controllers
+{
+    [Route("api/[controller]")]
+    [AllowAnonymous]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private IUserService _userService;
+
+        public UserController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpPost("[action]")]
+        public async Task<IActionResult> Login([FromBody] LoginModel login)
+        {
+            if (!ModelState.IsValid) return BadRequest("Invalid credentials");
+            var token = await _userService.GetToken(login);
+            return string.IsNullOrEmpty(token) ? BadRequest("Invalid email or password") : Ok(token);
+        }
+
+        [HttpPost("[action]")]
+        public async Task<IActionResult> Register([FromBody] RegisterModel model)
+        {
+            if (!ModelState.IsValid) return BadRequest("Errors in registration data");
+            var success = await _userService.RegisterUser(model);
+
+            return success ? Ok("Registration successful") : BadRequest("Registration failed");
+        }
+    }
+}
---
+++ ChatAppBackend/ChatAppBackend/Services/RegisterModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+using ChatAppBackend.Controllers;
+
+namespace ChatAppBackend.Services;
+
+public class RegisterModel : LoginModel
+{
+    [MaxLength(50)]
+    [Required]
+    public string Username { get; set; }
+}
