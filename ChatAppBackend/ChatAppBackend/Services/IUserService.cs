c78838a94d0162a24fa9189ffee611662c54109c
Samuel Paulíny <pauliny@cngroup.dk>
2022-03-10 20:01:19 +0100

    add authentication add database chat history

---
+++ ChatAppBackend/ChatAppBackend/Services/IUserService.cs
@@ -0,0 +1,10 @@
+﻿using ChatAppBackend.Controllers;
+
+namespace ChatAppBackend.Services;
+
+public interface IUserService
+{
+    Task<string> GetToken(LoginModel login);
+    Task<bool> RegisterUser(RegisterModel model);
+
+}
